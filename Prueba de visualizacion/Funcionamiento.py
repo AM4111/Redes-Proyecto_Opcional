4476cc66455892ff9d4dfebb9b1fedc496827f4d
AM4111 <pauma1018@gmail.com>
2022-08-27 19:53:24 -0600

    .
    .

--- Prueba de visualizacion/Funcionamiento.py
+++ Prueba de visualizacion/Funcionamiento.py
@@ -16,24 +16,6 @@ class GameState():
         ["TB","CB","AB","QB","KB","AB","CB","TB"],
         ]
         
-    def mover_Ficha(self,intx,inty,nextx,nexty):
-        if()
-
-class Peon():
-    def __init__(self):
-        nombre= ""
-        posiX = 0
-        posiY = 0
-        color = "N"
-    def mover(self):
-        posiX+=0
-        posiY+=1
-    def comer(self,Enemigo):
-        if(self.color !=Enemigo.color):
-            if(self.posiX+1==Enemigo.posiX or self.posiX-1==Enemigo.posiX ):
-                if(self.posiY+1==Enemigo.posiY or self.posiY-1==Enemigo.posiY ):
-                    self.posiX=Enemigo.posiX
-                    self.posiY=Enemigo.posiY
 
 
 
